3547fd3bd0d99b422df15b817188e5eca558e9d8
armen-mesropyan <162302434+armen-mesropyan@users.noreply.github.com>
2026-09-10 15:51:56 +0400

    Add pointer assignments 1-5 with report

---
+++ assignement5.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+	char str[] = "Hello";
+	char *ptr = str;
+
+	while(*ptr != '\0')
+	{
+		printf("%c", *ptr);
+		ptr++;
+		
+	}
+
+	int count = 0;
+	ptr = str;
+
+	while(*(ptr + count) != '\0')
+	{
+		count++;
+	}
+
+	printf("\nNumber of characters in string: %d", count);
+		
+	return 0;
+}
